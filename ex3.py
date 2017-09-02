c8b08e26644e156e5d3d3d96a694cd9a4a24418b
W Chu <rtype@protonmail.ch>
2017-09-02 10:42:07 -0400

    Add ex1 to ex3

---
+++ ex3.py
@@ -0,0 +1,37 @@
+# print string
+print("I will now count my chickens:")
+
+# evaluate math expression then print string and result
+print("Hens", 25. + 30 / 6)
+# evaluate math expression then print string and result
+print("Roosters", 100. - 25 * 3 % 4)
+
+# print string
+print("Now I will count the eggs:")
+
+# evaluate math expression then print result
+print(3. + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# print string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# evaluate math expressions and comparison then print
+print(3. + 2 < 5 - 7)
+
+# evaluate math expression then print string and result
+print("What is 3 + 2?", 3. + 2)
+# evaluate math expression then print string and result
+print("What is 5 - 7?", 5. - 7)
+
+# print string
+print("Oh, that's why it's False.")
+
+# print string
+print("How about some more.")
+
+# evaluate comparison then print string and bool
+print("Is it greater?", 5. > -2)
+# evaluate comparison then print string and bool
+print("Is it greater or equal?", 5. >= -2)
+# evaluate comparison then print string and bool
+print("Is it less or equal?", 5. <= -2)
