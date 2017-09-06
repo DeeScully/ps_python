544c6fd5208f6125a9485baa2ba4007b43748862
W Chu <rtype@protonmail.ch>
2017-09-05 22:31:02 -0400

    adde ex11 to ex13

---
+++ ex11.py
@@ -0,0 +1,15 @@
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print("How much do you weigh?", end = ' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+
+age_2 = input('How old are you? ')
+height_2 = input('How tall are you? ')
+weight_2 = input('How much do you weigh? ')
+
+print(f'So, you\'re {age_2} old, {height_2} tall and {weight_2} heavy.')
---
+++ ex13.py
@@ -0,0 +1,21 @@
+from sys import argv
+# read the WYSS section for how to run this
+# script, first, second, third = argv
+#
+# print("The script is called:", script)
+# print("Your first variable is:", first)
+# print("Your second variable is:", second)
+# print("Your third variable is:", third)
+
+
+# script, meat, potatoe = argv
+# print('The script is:', script)
+# print('The meat is:', meat)
+# print('The potatoe is:', potatoe)
+
+script, genre, album, track, song = argv
+print('The script is:', script)
+print('The genre is:', genre)
+print('The album name is:', album)
+print('The track number is:', track)
+print('The song name is:', song)
