79e709b07b7d8191d9b055767c186de9a4b93adc
W Chu <rtype@protonmail.ch>
2017-09-05 21:40:19 -0400

    add ex7 to ex9

---
+++ ex7.py
@@ -0,0 +1,40 @@
+# print str
+print("Mary had a little lamb.")
+# str format then print
+("Its fleece was white as {}.".format('snow'))
+# print str
+print("And everywhere that Mary went.")
+# multipy str then print
+("." * 10) # what'd that do?
+
+# assign str to var
+end1 = "C"
+# assign str to var
+end2 = "h"
+# assign str to var
+end3 = "e"
+# assign str to var
+end4 = "e"
+# assign str to var
+end5 = "s"
+# assign str to var
+end6 = "e"
+# assign str to var
+end7 = "B"
+# assign str to var
+end8 = "u"
+# assign str to var
+end9 = "r"
+# assign str to var
+end10 = "g"
+# assign str to var
+end11 = "e"
+# assign str to var
+end12 = "r"
+
+# comment with warning
+# watch that comma at the end.  try removing it to see what happens
+# concat vars then print with end of white space
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+# concat vars then print
+print(end7 + end8 + end9 + end10 + end11 + end12)
