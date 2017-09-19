f1f1db1c6c9c37354a200e5e3dc6fb95b1a9cd54
W Chu <rtype@protonmail.ch>
2017-09-18 20:59:31 -0400

    revised hw4 with byte conv programme

--- hw4.py
+++ hw4.py
@@ -48,3 +48,39 @@ def odd_even_counter(val_1, val_2, val_3, val_4, val_5):
     return odd, even
 
 # print('odd|even numbers counts are', odd_even_counter(7, 19, 85, 22, 0))
+
+
+
+# convert bytes
+a = 'Kilobytes to Bytes', 1000
+b = 'Megabytes to Bytes', 1000000
+c = 'Terabytes to Bytes', 1000000000000
+d = 'Terabytes to Megabytes', 1000000
+
+print(f'''We'll help you convert bytes!
+      a.  {a[0]}
+      b.  {b[0]}
+      c.  {c[0]}
+      d.  {d[0]}
+      '''
+      )
+
+selection = input('Make your selection > ')
+selection = selection.lower()
+
+if selection not in('a', 'b', 'c', 'd'):
+    raise ValueError("Only input 'a', 'b', 'c' or 'd'.")
+if selection == 'a':
+    choice, convert_rate = a
+elif selection == 'b':
+    choice, convert_rate = b
+elif selection == 'c':
+    choice, convert_rate = c
+else: choice, convert_rate = d
+
+print(f'Excellent, you chose to convert {choice}')
+
+num_to_convert = input('How many would you like to convert? > ')
+num = float(num_to_convert)
+
+print(f'Converting {num} {choice} > {(num * convert_rate)}')
