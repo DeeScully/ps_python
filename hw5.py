ebe420f509ed9a4853da1568c32f225c7fa9b053
W Chu <rtype@protonmail.ch>
2017-09-24 18:43:33 -0400

    add ex34 and hw5

--- hw5.py
+++ hw5.py
@@ -1,5 +1,32 @@
+#ex 34
+# animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+#
+# The animal at 1 is a python3.6.
+# The third (3rd) animal is a peacock.
+# The first (1st) animal is a bear.
+# The animal at 3 is a kangaroo.
+# The fifth (5th) animal is a whale.
+# The animal at 2 is a peacock.
+# The sixth (6th) animal is a platypus.
+# The animal at 4 is a kangaroo.
+#
+# The first (1st) animal is at 0 and is a bear.
+# The second (2nd) animal is at 1 and is a python3.6.
+# The third (3rd) animal is at 2 and is a peacock.
+# The fourth (4th) animal is at 3 and is a kangaroo.
+# The fifth (5th) animal is at 4 and is a whale.
+# The sixth (6th) animal is at 5 and is a platypus.
+#
+# The animal at 0 and at -6 is the 1st animal and is a bear.
+# The animal at 1 and at -5 is the 2nd animal and is a python3.6.
+# The animal at 2 and at -4 is the 3rd animal and is a peacock.
+# The animal at 3 and at -3 is the 4th animal and is a kangaroo.
+# The animal at 4 and at -2 is the 5th animal and is a whale.
+# The animal at 5 and at -1 is the 6th animal and is a platypus.
+
 import math
 import datetime
+from time import sleep
 
 # how many seconds in 42 mins 42 secs
 def secs(m, s):
@@ -8,7 +35,7 @@ def secs(m, s):
     """
     return m * 60 + s
 
-print(secs(42, 42))
+#print(secs(42, 42))
 
 
 # how many miles in 10 klicks
@@ -18,7 +45,7 @@ def miles(klicks):
     """
     return klicks * 1.61
 
-print(miles(10))
+#print(miles(10))
 
 # how much is 83f converted to c
 def fahr_to_cel(fahr):
@@ -27,7 +54,7 @@ def fahr_to_cel(fahr):
     """
     return (fahr - 32) / (9 / 5)
 
-print(fahr_to_cel(83))
+#print(fahr_to_cel(83))
 
 
 # sqrt of [81, 19, 16, 121]
@@ -38,8 +65,8 @@ def squareroot(num):
     return math.sqrt(num)
 
 num_list = [81, 19, 16, 121]
-for num in num_list:
-    print(squareroot(num))
+#for num in num_list:
+#    print(squareroot(num))
 
 
 # area of circ r = 9
@@ -49,20 +76,20 @@ def circ_area():
     """
     rad_str = input('type in the number you want the area of > ')
     rad = float(rad_str)
-    return rad ** 2 * math.pi
+    return (rad ** 2) * math.pi
 
-print(circ_area())
+#print(circ_area())
 
 
 # bool if 'x' is in str
-def is_in():
+def x_in():
     """
     ask for num and return if 'x' is in it
     """
     word = input('What is your word you want to check for \'x\'? > ')
     return 'x' in word.lower()
 
-print(is_in())
+#print(x_in())
 
 
 # bool if str contains vowel
@@ -76,7 +103,8 @@ def vowel_in():
             return True
     return False
 
-print(vowel_in())
+#print(vowel_in())
+
 
 
 # What is the volume of a sphere with radius 5? The volume of a sphere with radius r is  (4/3)πr3
@@ -86,7 +114,7 @@ def sphere_vol(rad):
     """
     return (4 / 3) * math.pi * rad ** 3
 
-print(sphere_vol(5))
+#print(sphere_vol(5))
 
 
 # Write a boolean function that returns true if a given input is divisible by 3 or return false otherwise
@@ -104,7 +132,7 @@ def div_by_three():
 
     return num % 3 == 0
 
-print(div_by_three())
+#print(div_by_three())
 
 
 # Import data/time library and print out today's data and current time
@@ -118,7 +146,7 @@ def current_datetime():
     print(f'Date is {now[0:11]}')
     print(f'Time is {now[12:16]}')
 
-current_datetime()
+#current_datetime()
 
 
 # using the data time library, print out the current year
@@ -129,7 +157,7 @@ def current_yr():
     now = datetime.datetime.now()
     print(f'Year is {now.year}')
 
-current_yr()
+#current_yr()
 
 # write a function that counts how  many times the letter a is repeated in a given word (get the work from the user as an input)
 def a_count():
@@ -145,7 +173,7 @@ def a_count():
 
     return count
 
-print(a_count())
+#print(a_count())
 
 
 # write code that counts the number of letters in a word provided by the user
@@ -153,7 +181,7 @@ def ltr_count():
     word = input('What word do you want the letter count for? > ')
     return len(word)
 
-print(ltr_count())
+#print(ltr_count())
 
 # triangle test. If any of the three lengths is greater than the sum of the other two, then you cannot form a triangle.
 
@@ -181,4 +209,94 @@ def three_inputs():
 
     triangle_chk(s1, s2, s3)
 
-three_inputs()
+#three_inputs()
+
+
+# Countdown function
+def countdown(time):
+    ''' num -> None
+    counts down from given time unit. timer set to 300 miliseconds per unit.
+    '''
+    counter = time
+
+    for i in range(time):
+        print(f'Time go BOOM in t-{counter}')
+        counter -= 1
+        sleep(.3)
+    print('BOOOOOOOOOOooooooooommmm!!!')
+
+#countdown(20)
+
+
+#function that tell if the given input is even or odd
+def odd_even():
+    '''
+    gets int from user and prints whether int is odd or even
+    '''
+    num_str = input('Type in the integer that you want to determine is odd or even: ')
+
+    try:
+        num = int(num_str)
+    except:
+        print('Please only input integers.  Try again. \n')
+        odd_even()
+
+    if num == 0:
+        result = 'zero'
+    elif num % 2 == 0:
+        result = 'even'
+    else: result = 'odd'
+
+    print(f'Your number: {num} is {result}.')
+
+#odd_even()
+
+
+#length of str given as input
+def length():
+    '''str -> int
+    gets str from user and returns the length
+    '''
+    string = input('Type in the string that you want to determine the length of: ')
+    count = 0
+
+    for ltr in string:
+        count += 1
+
+    return count
+
+#print(length())
+
+
+
+#loop that prints one letter at time of a str given as input
+def letter_printer():
+    '''
+    for each letter of given str, print each letter in newline
+    '''
+    string = input('Type in the string that you want to print each letter of: ')
+
+    for ltr in string:
+        print(ltr)
+        sleep(.3)
+
+letter_printer()
+
+#print(secs(42, 42))
+#print(miles(10))
+#print(fahr_to_cel(83))
+#for num in num_list:
+#    print(squareroot(num))
+#print(circ_area())
+#print(x_in())
+#print(vowel_in())
+#print(sphere_vol(5))
+#print(div_by_three())
+#current_datetime()
+#current_yr()
+#print(a_count())
+#print(ltr_count())
+#three_inputs()
+#countdown(20)
+#odd_even()
+#print(length())
