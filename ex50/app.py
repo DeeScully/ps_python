6ccd8d9342cfe08016a2d5b6bfc1db48f2dd79c7
W Chu <rtype@protonmail.ch>
2017-10-21 00:41:57 -0400

    update ex50

--- ex50/app.py
+++ ex50/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     greeting = 'World'
-    return f'Hello, {greeting}!'
+    return render_template('index.html', greeting=greeting)
 
 if __name__ == '__main__':
     app.run()
