907f6f1b0b427a82a642478e4b789a3738e30a95
W Chu <rtype@protonmail.ch>
2017-10-29 19:58:51 -0400

    add like button

--- flask/hw8/views.py
+++ flask/hw8/views.py
@@ -1,10 +1,17 @@
 from . import app
 from flask import Flask, render_template, request
 
+like_count = 0
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods = ['POST', 'GET'])
 def about():
-    return render_template('about_me.html')
+    if request.method == 'POST':
+        global like_count
+        like_count += 1
+        print(f'count is now {like_count}.')
+        count = like_count
+    return render_template('about_me.html', new_count = count)
